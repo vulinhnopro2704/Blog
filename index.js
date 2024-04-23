d3e6df578985ad38929fdf8c2ffb1f6094f71562
vulinhnopro2704 <vulinhtruong79@gmail.com>
2024-04-23 09:52:19 +0700

    modify index.js

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
 //localhost: 127.0.0.1
+//Khởi chạy: node index.js
